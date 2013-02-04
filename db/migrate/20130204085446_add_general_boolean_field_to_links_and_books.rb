e9f9c1e0b80cfb641f7e225c5e9a50d0b65cbd64
David Frey <david.michael.frey@gmail.com>
2013-02-04 03:56:18 -0500

    migration fix

--- db/migrate/20130204085446_add_general_boolean_field_to_links_and_books.rb
+++ db/migrate/20130204085446_add_general_boolean_field_to_links_and_books.rb
@@ -1,4 +1,4 @@
-class AddGeneralBooleanToLinksAndBooks < ActiveRecord::Migration
+class AddGeneralBooleanFieldToLinksAndBooks < ActiveRecord::Migration
   def change
     add_column :links, :general, :boolean, default: true
     add_column :books, :general, :boolean, default: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130204051019) do
+ActiveRecord::Schema.define(:version => 20130204085446) do
 
   create_table "actions", :force => true do |t|
     t.integer  "herb_id"
