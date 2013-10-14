602d660092f4c8fe5df11ba99d5a65cf14b8f42f
David Frey <perfectritone@gmail.com>
2013-10-14 18:46:35 -0400

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Earthroots::Application.config.secret_token = '544c5b1f723ce6527946b286d05437fe57ced5579be992dff8affe59eade4cbe9e60c668545cde4e05d05165b40a12a49594ca29b277db56ab7584786246ac84'
